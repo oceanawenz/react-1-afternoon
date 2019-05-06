26b261f1650ac4769e41117aef560fe8b61a1612
Oceana Wenz <oceana.wenz@gmail.com>
2019-05-06 16:30:04 -0700

    done

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export default class FilterObject extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            employees: [
+                {
+                    name: 'Michael Wenz',
+                    title: 'Graphic Desinger',
+                    age: 27,
+                },
+                {
+                    name: 'Oceana Wenz',
+                    age: 26,
+                    hairColor: 'brown'
+                },
+                {
+                    name: 'Domonic Wenz',
+                    title: 'Operator',
+                }
+            ],
+
+            userInput: '',
+            filteredEmployees: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    filterEmployees(prop) {
+        let employees = this.state.employees;
+        let filteredEmployees = [];
+
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].hasOwnProperty(prop)) {
+                filteredEmployees.push(employees[i]);
+            }
+        }
+
+        this.setState({ filteredEmployees: filteredEmployees });
+    }
+
+
+
+
+
+    render() {
+        return (
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Original: {JSON.stringify(this.state.employees, null, 10)}</span>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={() => this.filterEmployees(this.state.userInput)}>Filter</button>
+                <span className="resultsBox filterObjectRB">Filtered: {JSON.stringify(this.state.filteredEmployees, null, 10)}</span>
+            </div>
+        )
+    }
+}
